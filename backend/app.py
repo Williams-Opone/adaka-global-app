8e098620a307b1c088bd389545827a27d452caf0
fendyboy <oponeboboola@gmail.com>
2026-04-26 22:11:07 +0100

    Added a debug to app.py for the 500 internal error3

--- backend/app.py
+++ backend/app.py
@@ -126,6 +126,5 @@ def login():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-   
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
